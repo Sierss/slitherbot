12a45a4ad102542c4e99f1b11ddeae9ca8dd0ae4
icyicicle <yeskindofday+github@gmail.com>
2016-06-03 12:01:18 -0700

    snakehead angles and closest food into function
    Now draw which way the snakes are facing, and which way they are wanting to move.
    Turned the closest food method into a function

--- inject.js
+++ inject.js
@@ -24,6 +24,9 @@ var redraw = function() {
 	for(var i=0,max=snakes.length;i<max;i++) {
 		var snake = snakes[i];
 
+		//draw head angle
+		getsnakeheadangle(c,snake);
+
 		//the accurate snake head position reconstruction
 		//now longer experimental -- implementing line 2022 beautified from redraw() render_mode == 1; recompiling snake position accurately
 		var x = mx + (snake.xx + snake.fx - view_xx) * gsc; // line 2036
@@ -36,7 +39,7 @@ var redraw = function() {
 		c.moveTo(x, y);
 
 		//draw the tail of each snake
-		for (var f=snake.pts.length-1,fmin=0;f>=fmin&&f>=snake.pts.length-Math.floor(snake.cfl);f--) {
+		for (var f=snake.pts.length-1,fmin=0;f>=fmin&&f>=snake.pts.length-snake.cfl;f--) {
 			//EXPERIMENTAL -- snake head method does not seem to work out the same way for the tail
 			//todo: reverse engineer this line of code: 2047 
 			var pt = snake.pts[f];
@@ -61,18 +64,9 @@ var redraw = function() {
 
 		foodrel.push([x,y]);
 	}
+
 	//find the closest food
-	closestfood = [];
-	var closestdistance = 10000;
-	for (var i=0,max=foodrel.length;i<max;i++){
-		var x = foodrel[i][0];
-		var y = foodrel[i][1];
-		var distance = Math.sqrt(Math.pow(x-mx,2)+Math.pow(y-my,2));
-		if (closestdistance > distance) {
-			closestfood = [x,y];
-			closestdistance = distance;
-		}
-	}
+	closestfood = findclosestfood(foodrel, mx, my);
 
 	//draw line to closest food
 	c.strokeStyle="#FFFFFF";
@@ -85,13 +79,57 @@ var redraw = function() {
 	movemouse(closestfood[0],closestfood[1]);
 }
 
+function findclosestfood(foodscreenpos, x, y) {
+	var closestfood = [];
+	var closestdistance = 10000;
+	for (var i=0,max=foodscreenpos.length;i<max;i++){
+		var foodx = foodscreenpos[i][0];
+		var foody = foodscreenpos[i][1];
+		var distance = Math.sqrt(Math.pow(foodx-x,2)+Math.pow(foody-y,2));
+		if (closestdistance > distance) {
+			closestfood = [foodx,foody];
+			closestdistance = distance;
+		}
+	}
+	return closestfood;
+}
+function getsnakeheadangle(c,snake) {
+	//mid of screen
+	var mx = mc.width/2;
+	var my = mc.height/2;
 
+	//radius of the line coming out of the snakes head for direction pointer
+	var r = 100;
+
+	//snake head
+	var x = mx + (snake.xx + snake.fx - view_xx) * gsc;
+	var y = my + (snake.yy + snake.fy - view_yy) * gsc;
+	var x1 = x+r*Math.cos(snake.wehang);
+	var y1 = y+r*Math.sin(snake.wehang);
+	var x2 = x+r*Math.cos(snake.wang);
+	var y2 = y+r*Math.sin(snake.wang);
+	
+	//draw the line
+	c.strokeStyle="#FFFFFF";
+	c.beginPath();
+	c.moveTo(x,y);
+	c.lineTo(x1,y1);
+	c.stroke();
+	//what
+	c.strokeStyle="#FF0000";
+	c.beginPath();
+	c.moveTo(x,y);
+	c.lineTo(x2,y2);
+	c.stroke();
+
+}
 window.onmousemove = function() {
 	//original code is over written for the bot
 	/*window.onmousemove = function(b) {
 	 (b = b || window.event) && "undefined" != typeof b.clientX && (xm = b.clientX - ww / 2, ym = b.clientY - hh / 2)
 	};*/
 };
+
 //control mouse 
 movemouse = function(x,y) {
 	sx = x*window.innerWidth/mc.width;
